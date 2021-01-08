5905b7d8856f52bc65c289a6936b2acbbbeaa6fb
Mephis28 <sergei.krn@gmail.com>
2021-01-08 19:23:48 +0300

    init: create task2

---
+++ task2/src/components/ReactPureComp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ReactComp extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                Hello React!
+            </div>
+        );
+    }
+}
